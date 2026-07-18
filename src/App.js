4368ad1ad5f2528a170c80853503bd46bce34082
dasoelgit <dasoel@gmail.com>
2026-07-18 18:59:16 +0700

    Add useEffect import to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { createClient } from '@supabase/supabase-js';
 
 // ─── BADMINTON SUPABASE ──────────────────────────────────────────────────
